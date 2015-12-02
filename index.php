2a5e46900e4d9ab3a4bb2ae85f4b1d4f3ab96df9
diegocepedaw <diegocepedaw@gmail.com>
2015-12-02 14:26:27 -0500

    added tag to field

--- index.php
+++ index.php
@@ -37,9 +37,11 @@
         <input name="name" id="name" class="skipEnter" style="width: 300px;">
     </div>
 	
-	<label for="phoneNumber">Phone Number
-       <input type="text" name="phoneNumber" id="phoneNumber" placeholder="ex: 3855550168" required pattern="^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$"/>
-    </label>
+	<div class="ui-widget">
+		<label for="phoneNumber">Phone Number
+		   <input type="text" name="phoneNumber" id="phoneNumber" placeholder="ex: 3855550168" required pattern="^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$"/>
+		</label>
+	</div>
 
 	<div class="ui-widget">
 		<label for="school">School:</label>
