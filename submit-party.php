69915add1afa3c9a0c30b89b41398f7ae5496908
Carson Hynes <carsonhynes@gmail.com>
2015-12-04 15:20:30 -0500

    stuff

--- submit-party.php
+++ submit-party.php
@@ -3,6 +3,7 @@
 //mysqli('servername', 'username', 'password', 'database')
 $mysqli = new mysqli("localhost", "websys", "websys", "partylog");
 
+$username = "Iota Tau";
 
 $q = "('";
 
@@ -16,9 +17,11 @@ function test_input($data) {
 $q .= test_input($_POST["name"]) . "', '";
 $q .= test_input($_POST["fraternity"]) . "', '";
 $q .= test_input($_POST["school"]) . "', '";
+$q .= test_input($_POST["phoneNumber"]) . "', '";
+$q .= $username. "', '";
 
-if (test_input($_POST["over"] == NULL)) {
-	$q .= "Under', '";
+if (!isset($_POST["over"])) {
+	$q .= "Under')";
 }
 
 else {
@@ -27,7 +30,7 @@ else {
 
 //echo $q;
 
-$result = $mysqli->query("INSERT INTO party (name, fraternity, school, over) VALUES $q");
+$result = $mysqli->query("INSERT INTO party (name, fraternity, school, over, phone, username) VALUES $q");
 
 header("Location: index.php");
 
