a4e0659a893eaeadac3fd7bb6bdee69cec3ceedb
Conrad Mossl <conradmossl@gmail.com>
2015-12-07 14:58:13 -0500

    Upload link and menu changes
    Added an upload link and lookup link to the menu that will show when a
    user is logged in.

--- lookup.php
+++ lookup.php
@@ -98,8 +98,16 @@ try {
 </head>
 
 <body>
-
-<h1>Database Lookup</h1>
+	<menu>
+		<?php session_start(); if(isset($_SESSION['username'])) echo "<p> Welcome " . htmlentities($_SESSION['username']) ."</p>";?>
+		<ul>
+			<li id="title"><strong><a href="index.php">Party Log</a></strong></li>
+			<li><a href="login.php"><?php echo (isset($_SESSION['username'])) ? "Logout" : "Login";?></a></li>
+			<li><a href="mailto:carsonhynes@gmail.com?Subject=Party%20Log" target="_top">Contact</a></li>
+			<?php if(isset($_SESSION['username'])) echo "<li><a href='upload.php'>Upload</a><li><li><a href='lookup.php'>Lookup</a><li>";?>
+		</ul>
+	</menu>
+	<h1>Database Lookup</h1>
 
 <form action="lookup.php" method="post" onsubmit="return validate(this)">
 
