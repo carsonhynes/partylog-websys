2c742b77ce01eb27b770b09ece359f6aa9ee0b00
Buno Harvey <brunohrv@gmail.com>
2015-12-10 13:10:22 -0500

    whitespace

--- submit-party.php
+++ submit-party.php
@@ -64,65 +64,64 @@ $sc .= "'";
 
 
 $result = $mysqli->prepare("SELECT * FROM fraternity, school WHERE fraternity.name = $frat AND school.name = $sc");
+
 $result->execute();
 
 $result->store_result();
 
 if($result->num_rows > 0 ){
 
-$q .= test_input($_POST["name"]) . "', '";
-$q .= test_input($_POST["fraternity"]) . "', '";
-$q .= test_input($_POST["school"]) . "', '";
-$q .= test_input($_POST["phoneNumber"]) . "', '";
-$q .= $username. "', '";
+	$q .= test_input($_POST["name"]) . "', '";
+	$q .= test_input($_POST["fraternity"]) . "', '";
+	$q .= test_input($_POST["school"]) . "', '";
+	$q .= test_input($_POST["phoneNumber"]) . "', '";
+	$q .= $username. "', '";
 
-if (!isset($_POST["over"])) {
-	$q .= "Under')";
-}
-
-else {
-	$q .= "Over')";
-}
-
-echo $q;
-
-$result = $mysqli->query("INSERT INTO party (name, fraternity, school, phone, username, over) VALUES $q");
-
-
-
- //this sends out an sms to the phone number
-
-	if (isset($_POST["phoneNumber"]))
-	{
-
-
-		require 'PHPMailer/PHPMailerAutoload.php';
-
-
-		$number = $_POST["phoneNumber"];
-		$soberD = "4444444444"; // should be loaded from database by a query
-
-
-		//using carriers email to sms clients because it's the only free alternative to sms gateways
-		$email = $number ."@vtext.com";
-		$name = "Party Guest";
-		sendMail($email, $name, $soberD);
-
-		$email = $number ."@txt.att.net";
-		$name = "Party Guest";
-		sendMail($email, $name, $soberD);
-
-		$email = $number ."@tmomail.net";
-		$name = "Party Guest";
-		sendMail($email, $name, $soberD);
-
-		$email = $number ."@sprintpcs.com";
-		$name = "Party Guest";
-		sendMail($email, $name, $soberD);
-
-
+	if (!isset($_POST["over"])) {
+		$q .= "Under')";
 	}
 
-
+	else {
+		$q .= "Over')";
 	}
+
+	$result = $mysqli->query("INSERT INTO party (name, fraternity, school, phone, username, over) VALUES $q");
+
+
+
+ 	//this sends out an sms to the phone number
+
+		if (isset($_POST["phoneNumber"]))
+		{
+
+
+			require 'PHPMailer/PHPMailerAutoload.php';
+
+
+			$number = $_POST["phoneNumber"];
+			$soberD = "4444444444"; // should be loaded from database by a query
+
+
+			//using carriers email to sms clients because it's the only free alternative to sms gateways
+			$email = $number ."@vtext.com";
+			$name = "Party Guest";
+			sendMail($email, $name, $soberD);
+
+			$email = $number ."@txt.att.net";
+			$name = "Party Guest";
+			sendMail($email, $name, $soberD);
+
+			$email = $number ."@tmomail.net";
+			$name = "Party Guest";
+			sendMail($email, $name, $soberD);
+
+			$email = $number ."@sprintpcs.com";
+			$name = "Party Guest";
+			sendMail($email, $name, $soberD);
+
+
+		}
+
+
+}
 	header("Location: index.php");
