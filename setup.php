2df9ab6f6ba5751995a856a47dacae8b2c13eb0d
Buno Harvey <brunohrv@gmail.com>
2015-12-10 13:02:06 -0500

    added users table creation and db creation

--- setup.php
+++ setup.php
@@ -1,14 +1,35 @@
 	<?php
 
-
+		$host = 'localhost';
     $dbname = 'partylog';
-    $user = 'root';
-    $pass = '';
-    $dbconn = new PDO('mysql:host=localhost;dbname='.$dbname, $user, $pass);
+    $user = 'websys';
+    $pass = 'websys';
 
+		$dbh = new PDO("mysql:host=$host", $user, $pass);
+
+		$result = $dbh->prepare('CREATE DATABASE IF NOT EXISTS `partylog`
+																DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;');
+
+		$result->execute();
+
+    $dbconn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+		$result = $dbconn->prepare('CREATE TABLE IF NOT EXISTS `users` (
+									`id` int(11) NOT NULL AUTO_INCREMENT,
+									`username` varchar(50) NOT NULL,
+									`salt` varchar(100) NOT NULL,
+									`password` varchar(100) NOT NULL,
+									`frat` varchar(50) NOT NULL,
+									`school` varchar(50) NOT NULL,
+									`phone` int(11) DEFAULT NULL,
+									`over` varchar(10) DEFAULT \'under\',
+									PRIMARY KEY (`id`));
+								');
+
+		$result->execute();
 
     $result = $dbconn->prepare('CREATE TABLE IF NOT EXISTS `fraternity` (
-                 
+
                   `name` varchar(50) NOT NULL,
                   PRIMARY KEY (`name`));
                 ');
@@ -16,29 +37,29 @@
 
 
      $result = $dbconn->prepare('CREATE TABLE IF NOT EXISTS `school` (
-              
+
                   `name` varchar(50) NOT NULL,
                   PRIMARY KEY (`name`));');
     $result->execute();
-		
-		$schools = array("RPI", "Sage","UAlbany","Siena","Hudson Valley","Other");	
 
-	
+		$schools = array("RPI", "Sage","UAlbany","Siena","Hudson Valley","Other");
 
-		$greeks =array("Non-Greek","Acacia", "Alpha Chi Rho","Alpha Epsilon Pi", "Alpha Phi Alpha", "Alpha Sigma Phi","Chi Phi","Delta Kappa Epsilon","Delta Phi","Delta Tau Delta","Lambda Chi Alpha","Lambda Upsilon Lambda","Phi Gamma Delta", "Phi Iota Alpha", "Phi Kappa Tau", "Phi Kappa Theta", "Phi Mu Delta", "Phi Sigma Kappa", "Pi Delta Psi", "Pi Kappa Alpha", "Pi Kappa Phi", "Pi Lambda Phi", "Psi Upsilon", "RSE", "Sigma Alpha Epsilon", "Sigma Chi", "Sigma Alpha Epsilon", "Sigma Phi Epsilon", "Tau Epsilon Phi", "Theta Chi", "Theta Xi", "Zeta Psi");
+
+
+		$greeks =array("None","Acacia", "Alpha Chi Rho","Alpha Epsilon Pi", "Alpha Phi Alpha", "Alpha Sigma Phi","Chi Phi","Delta Kappa Epsilon","Delta Phi","Delta Tau Delta","Lambda Chi Alpha","Lambda Upsilon Lambda","Phi Gamma Delta", "Phi Iota Alpha", "Phi Kappa Tau", "Phi Kappa Theta", "Phi Mu Delta", "Phi Sigma Kappa", "Pi Delta Psi", "Pi Kappa Alpha", "Pi Kappa Phi", "Pi Lambda Phi", "Psi Upsilon", "RSE", "Sigma Alpha Epsilon", "Sigma Chi", "Sigma Alpha Epsilon", "Sigma Phi Epsilon", "Tau Epsilon Phi", "Theta Chi", "Theta Xi", "Zeta Psi");
 		foreach($schools as $school){
-			
+
 			$result =$dbconn->prepare('INSERT INTO school (name) VALUES (:school)');
 			$result->execute(array(':school' => $school));
 
-	
+
 		}
 		foreach($greeks as $greek){
 			$nextresult = $dbconn->prepare('INSERT INTO fraternity (name) VALUES (:greek)');
 			$nextresult->execute(array(':greek' => $greek));
 		}
 
+		header("Location: index.php");
 
-    
 
 	?>
